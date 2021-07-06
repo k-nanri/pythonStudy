fe03c4997cb9cd9ba4b719a9702dbd097ff9b661
k-nanri <kotaro.nanri15@gmail.com>
2021-07-07 08:00:59 +0900

    7/7 update

--- src/study.py
+++ src/study.py
@@ -1,5 +1,12 @@
+import sys
+
 print("Hello World")
 
-string = input('input your name:')
-message = "Hallo " + message + "!"
-print(message)
+
+args = sys.argv
+
+# スクリプト名
+print(args[0])
+# 1番目の引数
+print(args[1])
+print(args[2])
