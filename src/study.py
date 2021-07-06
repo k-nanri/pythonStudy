1ef612f1c1b37ea5def7bf0baf6defbc592a9b4b
k-nanri <kotaro.nanri15@gmail.com>
2021-07-07 08:03:10 +0900

    update

--- src/study.py
+++ src/study.py
@@ -3,10 +3,19 @@ import sys
 print("Hello World")
 
 
-args = sys.argv
+if __name__ == '__main__':
 
-# スクリプト名
-print(args[0])
-# 1番目の引数
-print(args[1])
-print(args[2])
+    args = sys.argv
+
+    if len(args) == 3:
+    
+        # スクリプト名
+        print(args[0])
+        # 1番目の引数
+        print(args[1])
+        print(args[2])
+
+    else:
+        print('以下の形式でパラメータを指定すること')
+        print('$ study.py aaa bbb')
+        quit()
