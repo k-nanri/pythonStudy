0cae21177783d4d44d078fcf66d1350bbffa425b
k-nanri <kotaro.nanri15@gmail.com>
2021-07-19 07:25:37 +0900

    7/19 update

---
+++ src/str_study.py
@@ -0,0 +1,19 @@
+
+if __name__ == '__main__':
+
+    text = "得意なプログラミング言語は\"python\"です。"
+    print(text)
+
+    # 文字列の結合
+    text1 = "今日は"
+    text2 = "いい天気"
+    text3 = text1 + text2
+    print(text3)
+
+    num = 5
+    text = "text"
+
+    print(num)
+    print(text + str(num))
+
+    
