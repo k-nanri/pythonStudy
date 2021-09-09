faebbee9b43a9426cfe7ef040faae2655df6fc5d
k-nanri <kotaro.nanri15@gmail.com>
2021-09-09 22:49:54 +0900

    9/9 update

--- src/redis/redis_main.py
+++ src/redis/redis_main.py
@@ -3,9 +3,15 @@ import redis
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 r.set('key01', 'aaaa')
+r.setnx('key01', 'bbbb')
 
 v = r.get('key01')
-
 v_str = v.decode()
+print(v_str)
+
+r.setnx('key02', 'bbbb')
+v = r.get('key02')
+v_str = v.decode()
+print(v_str)
 
 r.delete('key01')
