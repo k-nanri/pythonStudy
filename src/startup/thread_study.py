6ce9c5c6f30249d8b5e253ebff468dfd361b267f
k-nanri <kotaro.nanri15@gmail.com>
2021-10-01 07:56:21 +0900

    10/1 update

--- src/startup/thread_study.py
+++ src/startup/thread_study.py
@@ -1,16 +1,33 @@
-import threading
 import time
+import threading
+from concurrent.futures import ThreadPoolExecutor
+from logging import StreamHandler, Formatter, INFO, getLogger
+
+
+def init_logger():
+    handler = StreamHandler()
+    handler.setLevel(INFO)
+    handler.setFormatter(Formatter("[%(asctime)s] [%(threadName)s] %(message)s"))
+    logger = getLogger()
+    logger.addHandler(handler)
+    logger.setLevel(INFO)
+
+def task(v):
+
+    getLogger().info("%s start", v)
+    time.sleep(1.0)
+    getLogger().info("%s end", v)
 
 def execute_thread1():
 
     print("thread1 start")
-    time.sleep(10)
+    time.sleep(5)
     print("thread1 end")
 
 def execute_thread2():
 
     print("thread2 start")
-    time.sleep(5)
+    time.sleep(3)
     print("thread2 end")
 
 if __name__ == '__main__':
@@ -31,3 +48,14 @@ if __name__ == '__main__':
 
     # ThreadPoolExecutorの実装
     # Python3.2 から追加されたconcurrent.cuturesモジュールを使用する
+    init_logger()
+    getLogger().info("main start")
+    with ThreadPoolExecutor(max_workers=2, thread_name_prefix="thread") as executor:
+        
+        for i in range(5):
+            executor.submit(task, i)
+            getLogger().info("submit end")
+            getLogger().info("main end")
+
+
+
