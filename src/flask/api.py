de98ad8b08aab376e3adf8b4f3cd8847fe0167f2
k-nanri <kotaro.nanri15@gmail.com>
2021-10-06 23:07:14 +0900

    10/6 update

---
+++ src/flask/api.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, jsonify, abort, make_response
+import peewee
+
+db = peewee.SqliteDatabase("src/flask/data.db")
+
+class User(peewee.Model):
+    userId = peewee.TextField()
+    userCompany = peewee.TextField()
+    userDiscountRate = peewee.IntegerField()
+
+    class Meta:
+        database = db
+
+api = Flask(__name__)
+
+@api.route("/Users/<string:userId>", methods=["GET"])
+def get_user(userId):
+    try:
+        user = User.get(User.userId == userId)
+    except User.DoesNotExist:
+        abort(404)
+
+    result = {
+        "result": True,
+        "data": {
+            "userId": user.userId,
+            "userCompany": user.userCompany,
+            "userDiscountRate": user.userDiscountRate
+        }
+    }
+
+    return make_response(jsonify(result))
+
+@api.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({"error": "Not found"}), 404)
+
+if __name__ == "__main__":
+    api.run(host="0.0.0.0", port=3000)
+    
