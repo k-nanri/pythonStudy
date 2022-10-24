dd5c955c98137c117d396cc5c6d154fa57ae6df2
k-nanri <kotaro.nanri15@gmail.com>
2022-10-24 07:41:12 +0000

    Adding tarfile member sanitization to extractall()

--- src/tools/main.py
+++ src/tools/main.py
@@ -85,7 +85,26 @@ if __name__ == '__main__':
     # tarの操作は？
     os.chdir("test1")
     with tarfile.open("../../src/tools/hogehoge.tgz", "r:gz") as t:
-        t.extractall()
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(t)
         # hogeディレクトリ配下にtarファイルを展開
         # t.extractall(path="hoge")
 
