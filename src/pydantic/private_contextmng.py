bd6874e9518237ece41d7f763dc470b13b21689a
k-nanri <kotaro.nanri15@gmail.com>
2024-01-15 22:47:41 +0900

    Update

--- src/pydantic/private_contextmng.py
+++ src/pydantic/private_contextmng.py
@@ -5,11 +5,12 @@ def sample_context_mng():
     print("Start sample_context_mng")
     try:
         print("Call yield")
-        yield
+        yield "hoge"
     finally:
         print("Finally")
 
-with sample_context_mng():
+with sample_context_mng() as aaa:
     print("Start!!")
+    print("Value = " + str(aaa))
 
 print("End!!")
