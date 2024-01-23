462ee9e1aa352ee7921abce64b65c52fe52fec3c
k-nanri <kotaro.nanri15@gmail.com>
2024-01-24 08:05:18 +0900

    update

--- src/db_sqlalchemy/sample.py
+++ src/db_sqlalchemy/sample.py
@@ -2,24 +2,28 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import Column
 from sqlalchemy.types import Integer, String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import sessionmaker
-
-# DBエンジンを作成
-engine = create_engine("postgresql://postgres:example@localhost:5432/postgres")
+from sqlalchemy import select
 
 # モデルベースクラスを作成
-Base = declarative_base()
+#Base = declarative_base() と同等(2.0から)
+class Base(DeclarativeBase):
+    pass
 
 # モデルベースを拡張して、ORMのデータクラスを作成
-class User(Base):
-    __tablename__ = "user"
-    user_id = Column(Integer, primary_key=True)
-    first_name = Column(String(255))
-    last_name = Column(String(255))
-    age = Column(Integer)
+# mapped_columnは2.0からの新機能
+class Product(Base):
+    __tablename__ = "product"
+    _id: Mapped[str] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    cost: Mapped[int] = mapped_column(Integer, nullable=False)
+    manufacturer: Mapped[str] = mapped_column(String(255), nullable=False)
 
-    def full_name(self):
-        return "{self.first_name} {self.last_name}"
+# DBエンジンを作成
+url = "postgresql://postgres:example@localhost:5432/postgres"
+engine = create_engine(url, echo=True)
 
 # テーブルをDBに作成
 Base.metadata.create_all(engine)
@@ -29,12 +33,18 @@ Base.metadata.create_all(engine)
 SessionClass = sessionmaker(engine)
 session = SessionClass()
 
-user_a = User(first_name="tanaka", last_name="hoge", age=30)
-session.add(user_a)
+# Insert
+product1 = Product(name="チョコ", cost=100, manufacturer="fuku")
+product2 = Product(name="ガム", cost=10, manufacturer="tama")
+
+session.add_all([product1, product2])
 session.commit()
 
-users = session.query(User).all()
-print(users[0].first_name)
+# 検索
+stmt = select(Product).where(Product.name.in_(["チョコ"]))
+products = session.scalar(stmt)
+for product in products:
+    print("Name = " + str(product._id))
 
 
 # https://qiita.com/arkuchy/items/75799665acd09520bed2
