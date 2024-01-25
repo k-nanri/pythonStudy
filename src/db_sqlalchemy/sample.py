58ec36fb48138db08342b148a14fce42724043d5
k-nanri <kotaro.nanri15@gmail.com>
2024-01-26 08:18:36 +0900

    update

--- src/db_sqlalchemy/sample.py
+++ src/db_sqlalchemy/sample.py
@@ -1,11 +1,9 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.schema import Column
 from sqlalchemy.types import Integer, String
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import select, ForeignKey
+from sqlalchemy import select
 
 # モデルベースクラスを作成
 #Base = declarative_base() と同等(2.0から)
@@ -14,19 +12,13 @@ class Base(DeclarativeBase):
 
 # モデルベースを拡張して、ORMのデータクラスを作成
 # mapped_columnは2.0からの新機能
-class Manufacturer(Base):
-    __tablename__ = "manufacturer"
-    id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(255), nullable=False)
-    address: Mapped[str] = mapped_column(String(1024), nullable=False)
-
 class Product(Base):
     __tablename__ = "product"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(255), nullable=False)
     cost: Mapped[int] = mapped_column(Integer, nullable=False)
-    manufacturer_id: Mapped[int] = mapped_column(ForeignKey("manufacturer.id"))
    
+
 
 # DBエンジンを作成
 url = "postgresql://postgres:example@localhost:5432/postgres"
@@ -41,13 +33,8 @@ SessionClass = sessionmaker(engine)
 session = SessionClass()
 
 # Insert
-manufacturer1 = Manufacturer(name="toky", address="tokyo")
-manufacturer2 = Manufacturer(name="kana", address="kanagawa")
-session.add_all([manufacturer1, manufacturer2])
-session.commit()
-
-product1 = Product(name="チョコ", cost=100, manufacturer_id=1)
-product2 = Product(name="ガム", cost=10, manufacturer_id=2)
+product1 = Product(name="チョコ", cost=100)
+product2 = Product(name="ガム", cost=10)
 session.add_all([product1, product2])
 session.commit()
 
@@ -55,15 +42,22 @@ session.commit()
 stmt = select(Product).where(Product.name.in_(["チョコ"]))
 products = session.scalars(stmt)
 for product in products:
-    print("Name = " + str(product.id))
+    print("Name = " + str(product.name))
 
-# 検索(Join)
-stmt = (
-        select(Product)
-        .join(Product.manufacturer_id)
-        .where(Product.name == "チョコ"))
+# 変更
+stmt = select(Product).where(Product.id == 1)
+product = session.scalars(stmt).one()
+product.name = "パン"
+session.commit()
 
-res = session.scalars(stmt).one()
+# 検索
+stmt = select(Product)
+products = session.scalars(stmt)
+for product in products:
+    print("Name = " + str(product.name))
+
+# 削除
+
 
 
 # https://qiita.com/arkuchy/items/75799665acd09520bed2
