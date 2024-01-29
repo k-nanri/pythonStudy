2bb069122ece8745a8155e3685cd7a0a69b44c05
k-nanri <kotaro.nanri15@gmail.com>
2024-01-30 08:09:29 +0900

    update

--- src/db_sqlalchemy/select_join.py
+++ src/db_sqlalchemy/select_join.py
@@ -16,6 +16,9 @@ class Student(Base):
     name: Mapped[str] = mapped_column(String(30))
     school_id: Mapped[int]
 
+    def __str__(self):
+        return "id = " + str(self.id) + ", name = " + str(self.name) + ", school_id = " + str(self.school_id)
+
 class School(Base):
     __tablename__ = "school"
     id: Mapped[int] = mapped_column(primary_key=True)
@@ -41,8 +44,12 @@ session.add(student)
 session.commit()
 
 # relationshipの設定をしていない場合はこれで操作できる
-stmt = select(Student).join(School, Student.school_id == School.id)
+stmt = select(Student.id, Student.name, School.name).join(School, Student.school_id == School.id)
 print(stmt)
+#results = session.scalars(stmt)
+results = session.execute(stmt).mappings().all()
+for result in results:
+    print(result)
 
 # https://docs.sqlalchemy.org/en/20/orm/queryguide/select.html#joins
 # https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html
