566d5cb021cb266e38a9a9a1fa0e4c6138c9aceb
k-nanri <kotaro.nanri15@gmail.com>
2024-01-29 23:12:40 +0900

    update

---
+++ src/db_sqlalchemy/select_join.py
@@ -0,0 +1,48 @@
+from sqlalchemy import create_engine
+from sqlalchemy.types import Integer, String
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+from sqlalchemy import select
+
+
+class Base(DeclarativeBase):
+    pass
+
+class Student(Base):
+    __tablename__ = "student"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    school_id: Mapped[int]
+
+class School(Base):
+    __tablename__ = "school"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+
+# DBエンジンを作成
+url = "postgresql://postgres:example@localhost:5432/postgres"
+engine = create_engine(url, echo=True)
+
+# テーブルをDBに作成
+Base.metadata.create_all(engine)
+
+# セッションを作成
+# セッションを介してクエリを実行する
+SessionClass = sessionmaker(engine)
+session = SessionClass()
+
+school = School(name="hoge")
+session.add(school)
+session.commit()
+student = Student(name="tanaka", school_id=1)
+session.add(student)
+session.commit()
+
+# relationshipの設定をしていない場合はこれで操作できる
+stmt = select(Student).join(School, Student.school_id == School.id)
+print(stmt)
+
+# https://docs.sqlalchemy.org/en/20/orm/queryguide/select.html#joins
+# https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html
