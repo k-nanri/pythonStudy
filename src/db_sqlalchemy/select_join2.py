0a0e662dacbe410d7034add8877af86ba756ba81
k-nanri <kotaro.nanri15@gmail.com>
2024-02-02 17:35:35 +0900

    update

--- src/db_sqlalchemy/select_join2.py
+++ src/db_sqlalchemy/select_join2.py
@@ -4,6 +4,7 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 from sqlalchemy import select
 
 
@@ -14,7 +15,8 @@ class Student(Base):
     __tablename__ = "student"
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
-    school_id: Mapped[int]
+    school_id: Mapped[int] = mapped_column(ForeignKey("school.id"))
+    school: Mapped["School"] = relationship()
 
 
     def __str__(self):
@@ -31,23 +33,34 @@ url = "postgresql://postgres:example@localhost:5432/postgres"
 engine = create_engine(url, echo=True)
 
 # テーブルをDBに作成
+#Base.metadata.create_all(engine)
 
 # セッションを作成
 # セッションを介してクエリを実行する
 SessionClass = sessionmaker(engine)
 session = SessionClass()
 
-
+school1 = School(name="あいうえお学園")
+school2 = School(name="かきくけこ学園")
+school3 = School(name="さしすせそ学校")
+session.add_all([school1, school2, school3])
+session.commit()
+student1 = Student(name="sato", school_id=1)
+student2 = Student(name="tanaka", school_id=2)
+student3 = Student(name="watanabe", school_id=3)
+session.add_all([student1, student2, student3])
 session.commit()
 
 # relationshipの設定をしていない場合はこれで操作できる
-stmt = select(Student.id, Student.name, School.name.label("school_name")).join(School, Student.school_id == School.id).where(Student.id == 1)
-print(stmt)
+stmt = select(Student, School).join(School, Student.school_id == School.id).where(Student.id == 1)
 results = session.scalars(stmt)
-#results = session.execute(stmt).mappings().all()
-print("results = " + str(results))
+
 for result in results:
-    print(result)
+    print("student.id = " + str(result.id))
+    print("student.name = " + str(result.name))
+    print("student.school_id = " + str(result.school_id))
+    print("student.school.id = " + str(result.school.id))
+    print("student.school.name = " + str(result.school.name))
 
 # https://docs.sqlalchemy.org/en/20/orm/queryguide/select.html#joins
 # https://docs.sqlalchemy.org/en/20/orm/basic_relationships.html
