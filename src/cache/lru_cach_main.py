061a20009d3c6d0cdc4c03129beaa07ca19cc1c2
k-nanri <kotaro.nanri15@gmail.com>
2024-02-24 20:38:13 +0900

    update

--- src/cache/lru_cach_main.py
+++ src/cache/lru_cach_main.py
@@ -1,18 +1,51 @@
 from functools import lru_cache
 
 @lru_cache(maxsize=2)
-def again():
-    print("hoge")
+def calculate(a):
+    print("-- Call calculate function")
     r = 0
-    for i in range(100000000):
+    for i in range(10000000):
         r += i
 
     return r
 
 def main_call():
-    for i in range(5):
-        print(again())
-        print(again.cache_info())
+
+    print("calculate 1")
+    calculate(1)
+    print(calculate.cache_info())
+
+    print("calculate 2")
+    calculate(2)
+    print(calculate.cache_info())
+
+    print("cacheが効いているはず -------")
+    print("calculate 1")
+    calculate(1)
+    print(calculate.cache_info())
+
+    print("calculate 2")
+    calculate(2)
+    print(calculate.cache_info())
+
+    print("1のキャッシュが削除 -------")
+    print("calculate 3")
+    calculate(3)
+    print(calculate.cache_info())
+
+    print("2と3のcacheが効いているはず -------")
+    print("calculate 2")
+    calculate(2)
+    print(calculate.cache_info())
+
+    print("calculate 3")
+    calculate(3)
+    print(calculate.cache_info())
+
+    print("1のcacheがない -------")
+    print("calculate 1")
+    calculate(1)
+    print(calculate.cache_info())
 
 if __name__ == '__main__':
     main_call()
