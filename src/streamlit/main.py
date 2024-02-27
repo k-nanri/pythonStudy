df225c33cc151160c430a7f366cd7751a9a9095d
k-nanri <kotaro.nanri15@gmail.com>
2024-02-28 06:55:10 +0900

    update

--- src/streamlit/main.py
+++ src/streamlit/main.py
@@ -5,7 +5,6 @@ from sqlalchemy.types import Integer, String, DateTime
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import select
 import datetime
 
 class Base(DeclarativeBase):
@@ -31,22 +30,21 @@ session = SessionClass()
 with st.form("my_form"):
     st.title("Let's try!!!")
     gender = st.radio("Gender", ["Male", "Female"])
-    age = st.number_input("Age", min_value=0, max_value=100, format="%d")
+    age = st.number_input("Age", min_value=20, max_value=60, format="%d")
     frequency_show = st.radio('映画はよく見ますか？', ["月5回以上", "月1回", "半年に1回", "見ない"])
     good_movie = st.slider("今回の映画の評価を教えてください(0-5)", 0, 5, 0)
 
     submitted = st.form_submit_button("送信")
     if submitted:
-        st.write("age", age, "good_movie", good_movie)
+        movie_questionnaire = Moviequestionnaire(time=datetime.datetime.now(),
+                                                 gender=gender,
+                                                 age=age,
+                                                 frequency_show=frequency_show,
+                                                 good_movie=good_movie)
+        session.add(movie_questionnaire)
+        session.commit()
 
-st.subheader("Search DB")
 
-stmt = select(Moviequestionnaire)
-questionnaires = session.scalars(stmt)
-for item in questionnaires:
-    print(item.time)
-    print(item.gender)
-print("OK")
 
 
 
