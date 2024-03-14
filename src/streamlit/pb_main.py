2f60c1fa8520adc40b5de4f5396387bee349200a
k-nanri <kotaro.nanri15@gmail.com>
2024-03-15 07:09:21 +0900

    update

--- src/streamlit/pb_main.py
+++ src/streamlit/pb_main.py
@@ -5,15 +5,13 @@ import numpy as np
 
 st.title("PB曲線グラフ")
 
-data = [[10, 10, 2], [5, 5, 8]]
+data = [[0, 10, 2], [5, 4, 8]]
 chart_data = pd.DataFrame(data=data, columns=["a", "b", "c"])
-data = [[3, 4], [5, 4]]
-bug = pd.DataFrame(data=data, columns=["a", "c"])
-print(chart_data)
-
-# c = alt.Chart(chart_data).mark_line().encode(x="a", y="b", tooltip=["a", "b"])
 base = alt.Chart(chart_data).encode(x="a")
-c = base.mark_line(color="red").encode(y="b")
-d = base.mark_line().encode(y2="c")
-
-st.altair_chart((c + d), use_container_width=True)
+c = base.mark_line(color="red").encode(
+    alt.Y("b", axis=alt.Axis(title="消化予定項目数"))
+)
+# d = base.mark_line().encode(y="c")
+d = base.mark_line().encode(alt.Y("c", axis=alt.Axis(title="bug")))
+e = (c + d).resolve_scale(y="independent")
+st.altair_chart(e, use_container_width=True)
