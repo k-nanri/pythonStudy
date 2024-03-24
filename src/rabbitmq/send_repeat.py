26d7d3c45bf4b5bf72500f429d2002b28ef55335
k-nanri <kotaro.nanri15@gmail.com>
2024-03-25 07:19:42 +0900

    update

--- src/rabbitmq/send_repeat.py
+++ src/rabbitmq/send_repeat.py
@@ -7,12 +7,17 @@ connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
 channel = connection.channel()
 
 # キューを作成
-channel.queue_declare(queue="hello")
+channel.queue_declare(queue="hello", durable=True)
 
 
 for i in range(50):
     message = "Message No." + str(i)
-    channel.basic_publish(exchange="", routing_key="hello", body=message)
+    channel.basic_publish(
+        exchange="",
+        routing_key="hello",
+        body=message,
+        properties=pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent),
+    )
     print("  [x] Send " + message)
     time.sleep(0.5)
 
