534c800cd6de5dd9f896f340e1e279d95255e578
k-nanri <kotaro.nanri15@gmail.com>
2024-04-25 06:56:24 +0900

    update

---
+++ src/rabbitmq/topic/emit_syslog.py
@@ -0,0 +1,16 @@
+import pika
+import time
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+channel = connection.channel()
+channel.exchange_declare(exchange="topic_logs", exchange_type="topic")
+
+routing_keys = ["syslog.debug", "syslog.debug.aaa"]
+
+for routing_key in routing_keys:
+    message = "topic=" + str(routing_key) + " message!!"
+    print("Send message [" + str(message) + "]")
+    channel.basic_publish(exchange="topic_logs", routing_key=routing_key, body=message)
+    time.sleep(1)
+
+connection.close()
--- src/rabbitmq/topic/receive_topic.py
+++ src/rabbitmq/topic/receive_topic.py
@@ -4,13 +4,19 @@ import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
-handler.setLevel(logger.DEBUG)
+handler.setLevel(logging.DEBUG)
 handler_format = logging.Formatter("%(asctime)s - %(message)s")
 handler.setFormatter(handler_format)
 logger.addHandler(handler)
 
-connection = pika.BlockkingConnection(pika.ConnectionParameters(host="localhost"))
-channel = connection.Channel()
+
+def callback(ch, method, properties, body):
+    logger.debug("=== callback log =========")
+    logger.debug(f"{method.routing_key}: {body}")
+
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
+channel = connection.channel()
 
 channel.exchange_declare(exchange="topic_logs", exchange_type="topic")
 
@@ -19,8 +25,8 @@ binding_keys = ["syslog.debug"]
 for binding_key in binding_keys:
     result = channel.queue_declare("", exclusive=True)
     queue_name = result.method.queue
-    channel.queue_bind(
-        exchange="topic_logs", queue_name=queue_name, routing_key=binding_key
-    )
+    channel.queue_bind(exchange="topic_logs", queue=queue_name, routing_key=binding_key)
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
 logger.info(" [*] Waitting for logs. To exit press CTRL+C")
+channel.start_consuming()
