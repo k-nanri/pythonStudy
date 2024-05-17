2ae63baf9a4b6e22e161733bc9b5d2b1487d36be
k-nanri <kotaro.nanri15@gmail.com>
2024-05-18 07:45:34 +0900

    update

--- src/fastapi/backgroundtask.py
+++ src/fastapi/backgroundtask.py
@@ -1,20 +1,61 @@
 from fastapi import BackgroundTasks, FastAPI
 import time
-from logging import getLogger, StreamHandler
+from logging import getLogger
+import uuid
+from uuid import UUID
+from typing import Union
+import asyncio
+from pydantic import BaseModel
 
+logger = getLogger("uvicorn.app")
 app = FastAPI()
 
-logger = getLogger(__name__)
-logger.addHandler(StreamHandler())
-logger.setLevel("INFO")
+collecting_status = []
 
 
-def execute_long_time(mode: str, message=""):
-    logger.info("--- START execute_log_time")
-    logger.info("--- params mode    = " + mode)
-    logger.info("--- params message = " + message)
-    time.sleep(60)
-    logger.info("--- END execute_log_time")
+class CollectRequest(BaseModel):
+    mode: str
+
+
+@app.get("/colect2")
+async def collect2():
+    logger.info("Start collect2")
+
+
+@app.get("/collect_status/{request_id}")
+async def collect_status(request_id, query: Union[str, None] = None):
+    logger.info("Start collect_status")
+    status = "complete"
+    logger.info("current collecting_status=" + str(collecting_status))
+    for item in collecting_status:
+        if item["request_id"] == request_id:
+            status = "collecting"
+            break
+
+    logger.info("End collect_status")
+    response = {"status": status}
+    return response
+
+
+async def execute_long_time(mode: str, request_id: str):
+    logger.info("Start execute_long_time method")
+    logger.info("execute_log_time param mode=" + mode + ", request_id=" + request_id)
+    collecting_status.append({"request_id": request_id, "status": "collecting"})
+    await asyncio.sleep(60)
+
+    if {"request_id": request_id, "status": "collecting"} in collecting_status:
+        collecting_status.remove({"request_id": request_id, "status": "collecting"})
+
+    logger.info("End execute_long_time method")
+
+
+@app.post("/collectlog")
+async def collect_log(request: CollectRequest, background_tasks: BackgroundTasks):
+    request_id = uuid.uuid4()
+    background_tasks.add_task(execute_long_time, request.mode, str(request_id))
+
+    response = {"request_id": request_id}
+    return response
 
 
 """
