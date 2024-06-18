2771b5888d6fbea34790fa63bd428d52385f6edb
k-nanri <kotaro.nanri15@gmail.com>
2024-06-19 06:25:43 +0900

    update

---
+++ src/fastapi/client/ws_client/src/App.jsx
@@ -0,0 +1,89 @@
+import React, { useState, useCallback, useEffect } from 'react';
+import useWebSocket, { ReadyState } from 'react-use-websocket';
+import { DataGrid, useGridApiRef } from '@mui/x-data-grid';
+
+const WS_URL = 'ws://127.0.0.1:8000/ws';
+
+// これを参考に
+// https://mui.com/x/react-data-grid/row-updates/#the-updaterows-method
+
+function App() {
+  //Public API that will echo messages sent to it back to the client
+  const [socketUrl, setSocketUrl] = useState(WS_URL);
+  const [messageHistory, setMessageHistory] = useState([]);
+  const apiRef = useGridApiRef();
+
+  const { sendMessage, lastJsonMessage, readyState } = useWebSocket(socketUrl);
+
+  // 受信したデータを設定する
+  useEffect(() => {
+    if (lastJsonMessage !== null) {
+      setMessageHistory((prev) => {
+        for (let i = 0; i < prev.length; i++) {
+          if (prev[i].message === lastJsonMessage.message) {
+            return prev;
+          }
+        }
+        prev.push(lastJsonMessage)
+        set
+        return prev;
+      })
+    }
+  }, [lastJsonMessage]);
+
+  const handleClickChangeSocketUrl = useCallback(
+    () => setSocketUrl(WS_URL),
+    []
+  );
+
+  const handleClickSendMessage = useCallback(() => sendMessage('Hello'), []);
+
+  const connectionStatus = {
+    [ReadyState.CONNECTING]: 'Connecting',
+    [ReadyState.OPEN]: 'Open',
+    [ReadyState.CLOSING]: 'Closing',
+    [ReadyState.CLOSED]: 'Closed',
+    [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
+  }[readyState];
+
+  const rows = [
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 3, col1: 'MUI', col2: 'is Amazing' },
+  ];
+  
+  const columns = [
+    { field: 'id', headerName: 'id', width: 150 },
+    { field: 'severity', headerName: 'Severity', width: 150 },
+    { field: "message", headerName: "Message", width: 300}
+  ];
+
+  return (
+    
+    <div>
+        <div style={{ height: 300, width: '100%' }}>
+      <DataGrid rows={rows} columns={columns} />
+    </div>
+      <button onClick={handleClickChangeSocketUrl}>
+        Click Me to change Socket Url
+      </button>
+      <button
+        onClick={handleClickSendMessage}
+        disabled={readyState !== ReadyState.OPEN}
+      >
+        Click Me to send 'Hello'
+      </button>
+      <span>The WebSocket is currently {connectionStatus}</span>
+      {lastJsonMessage ? <span>Last message: {lastJsonMessage.data}</span> : null}
+      <ul>
+        {
+          messageHistory.map((message, idx) => {
+          return (<li key={idx}><span key={idx}>{message ? message.message : null}</span></li>)
+          })
+        }
+      </ul>
+    </div>
+  );
+};
+
+export default App
